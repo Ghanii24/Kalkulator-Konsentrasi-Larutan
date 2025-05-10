5ef493b463ac0c9c03fb78cbf8768db134d031a7
Ghanii24 <rehanghanii04@gmail.com>
2025-05-10 22:57:38 +0700

    Update KALKULATOR KONSENTRASI LARUTAN.py

--- KALKULATOR KONSENTRASI LARUTAN.py
+++ KALKULATOR KONSENTRASI LARUTAN.py
@@ -14,7 +14,7 @@ st.markdown("""
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
-        color: white !important;
+        color: transparent !important;
     }
     </style>
     """, unsafe_allow_html=True)
